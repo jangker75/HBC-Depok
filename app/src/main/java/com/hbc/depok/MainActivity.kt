5baad9527896aabea0662f9d6c41cf3ab97e44e5
jangker75 <jangker_75@ymail.com>
2019-01-31 18:21:39 +0700

    -show list data (done) -show detail (half) -show data max per page (on progress) -show search button (on progress) -show login (on progress) -show splash screen (on progress)

--- app/src/main/java/com/hbc/depok/MainActivity.kt
+++ app/src/main/java/com/hbc/depok/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.hbc.depok
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
@@ -24,17 +26,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         usersAdapter = UsersAdapter()
-        rv_recycler.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        rv_recycler.layoutManager = LinearLayoutManager(this)
+        //rv_recycler.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
         rv_recycler.adapter = usersAdapter
 
+        fetchJson()
+    }
+
+    fun fetchJson() {
         val retrofit: Retrofit = Retrofit.Builder()
                 .baseUrl("http://hbcdepok.com/data/public/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
-
         val apiData = retrofit.create(ApiNetwork::class.java)
-
         apiData.getData()
                 .subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.computation())
@@ -44,6 +49,13 @@ class MainActivity : AppCompatActivity() {
                             Toast.makeText(applicationContext, it.message, Toast.LENGTH_SHORT).show()
                         })
     }
+    //   private fun partItemClicked(partItem : Data) {
+    //       Toast.makeText(this, "Clicked: ${partItem.kode_anggota}", Toast.LENGTH_LONG).show()
 
+    // Launch second activity, pass part ID as string parameter
+//        val showDetailActivityIntent = Intent(this, DetailMemberActivity::class.java)
+    //       showDetailActivityIntent.putExtra(Intent.EXTRA_TEXT, partItem.kode_anggota)
+    //       startActivity(showDetailActivityIntent)
+    //   }
 
 }
--- app/src/main/java/com/hbc/depok/UsersAdapter.kt
+++ app/src/main/java/com/hbc/depok/UsersAdapter.kt
@@ -1,25 +1,37 @@
 package com.hbc.depok
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_data_layout.view.*
 
-class UsersAdapter : RecyclerView.Adapter<UsersAdapter.DataViewHolder>() {
+class UsersAdapter : RecyclerView.Adapter<UsersAdapter.CustomViewHolder>() {
 
     private val data: MutableList<Data> = mutableListOf()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
-        return DataViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_data_layout, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CustomViewHolder {
+        val isiData = LayoutInflater.from(parent?.context).inflate(R.layout.item_data_layout, parent, false)
+        //   return CustomViewHolder(isiData)
+        return CustomViewHolder(isiData)
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
-    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
-        holder.bindModel(data[position])
+    override fun onBindViewHolder(holder: CustomViewHolder?, position: Int) {
+        val data = data.get(position)
+        holder?.view?.txtID?.text = data.kode_anggota
+        holder?.view?.txtNama?.text = data.Nama
+        holder?.view?.txtPlat?.text = data.No_Telp
+        // holder.bindModel(data[position])
+        val imgFoto1 = holder?.view?.imgView
+        Picasso.get().load(data.foto1).into(imgFoto1)
     }
 
     fun setData(datas: List<Data>) {
@@ -27,18 +39,24 @@ class UsersAdapter : RecyclerView.Adapter<UsersAdapter.DataViewHolder>() {
         notifyDataSetChanged()
     }
 
-    inner class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class CustomViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        init {
+            view.setOnClickListener {
+                val intent = Intent(view.context, DetailMemberActivity::class.java)
+                view.context.startActivity(intent)
+            }
 
-        val txtID: TextView = itemView.findViewById(R.id.txtID)
-        val txtNama: TextView = itemView.findViewById(R.id.txtNama)
-        val txtPlat: TextView = itemView.findViewById(R.id.txtPlat)
-        //    val movieAvatarImage : ImageView = itemView.findViewById(R.id.movieAvatar)
-
-        fun bindModel(data: Data) {
-            txtID.text = data.kode_anggota
-            txtNama.text = data.Nama
-            txtPlat.text = data.No_plat
-            //   Picasso.get().load(data.foto).into(imgView)
         }
     }
 }
+
+//class CustomViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+//    init {
+//       view.setOnClickListener {
+//           val intent = Intent(view.context, DetailMemberActivity::class.java)
+//          view.context.startActivity(intent)
+//       }
+
+//   }
+
+//}
